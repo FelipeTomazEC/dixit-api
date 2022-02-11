62377ee62f37809f486bcc1d22a36a93db4f2286
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-02-10 21:30:03 -0300

    feat: implement code generator using uuid

---
+++ src/infra/implementations/uuid-room-code-generator.ts
@@ -0,0 +1,8 @@
+import { MatchCodeGenerator } from '@use-cases/create-match/match-code-generator.interface';
+import { v4 as uuidV4 } from 'uuid';
+
+export class UUIDRoomCodeGenerator implements MatchCodeGenerator {
+  async generateCode(): Promise<string> {
+    return uuidV4();
+  }
+}
