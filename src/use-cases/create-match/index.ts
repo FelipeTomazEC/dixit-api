b568e27affecf10d2556376caa8f156f9892cc8d
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-02-11 08:22:01 -0300

    refactor: create common deps folder

--- src/use-cases/create-match/index.ts
+++ src/use-cases/create-match/index.ts
@@ -3,7 +3,7 @@ import { UseCaseInputPort } from '@use-cases/ports/usecase-input-port';
 import { UseCaseOutputPort } from '@use-cases/ports/usecase-output-port';
 import { CreateMatchResponse } from './response';
 import { MatchCodeGenerator } from './match-code-generator.interface';
-import { MatchRepository } from './match-repository.interface';
+import { MatchRepository } from '@use-cases/common-dependencies/match-repository.interface';
 
 interface Dependencies {
   codeGenerator: MatchCodeGenerator;
--- test/use-cases/create-match.test.ts
+++ test/use-cases/create-match.test.ts
@@ -1,8 +1,8 @@
 import { Match } from '@entities/match';
 import { getMock } from '@test/test-helpers/get-mock';
+import { MatchRepository } from '@use-cases/common-dependencies/match-repository.interface';
 import { CreateMatchUseCase } from '@use-cases/create-match';
 import { MatchCodeGenerator } from '@use-cases/create-match/match-code-generator.interface';
-import { MatchRepository } from '@use-cases/create-match/match-repository.interface';
 import { CreateMatchResponse } from '@use-cases/create-match/response';
 import { UseCaseOutputPort } from '@use-cases/ports/usecase-output-port';
 jest.mock('@entities/match');
