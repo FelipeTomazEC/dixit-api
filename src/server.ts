75aa4c8b936a56f64fb58cc546b9f449c40c6595
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-02-17 01:52:48 -0300

    chore: update env variable name

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { createExpressApplication } from '@infra/express/create-express-applicat
 
 const expressApp = createExpressApplication();
 const server = createServer(expressApp);
-const port = process.env.SERVER_PORT ?? 3000;
+const port = process.env.PORT ?? 3000;
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}.`);
